cf6587ae12edcb8a2c6d55e1ec1312a3f99a6f6c
Juan Gelabert <juanc.gelabert@gmail.com>
2023-02-19 14:02:18 -0300

    using loading State to render a Spinner while waiting the response of fetch

---
+++ src/components/Spinner/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spinner() {
+
+    return (
+        <div className='spin-wrapper'>
+            <div className='spinner'>
+            </div>
+        </div>
+    )
+}
